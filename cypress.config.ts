7477eb17bf161da79fdaaf87aa9da08b28867e36
apella1 <johncollons5@gmail.com>
2023-11-16 12:18:07 +0300

    Set up cypress github actions workflow

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  projectId: "k9twt2",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
